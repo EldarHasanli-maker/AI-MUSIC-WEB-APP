74a5531743c33784257bf16c8520c5a87313343e
EldarHasanli-maker <76447592+EldarHasanli-maker@users.noreply.github.com>
2021-08-30 12:46:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-song="";
+song1="";
+song2="";
 function preload(){
+    song1=loadSound("peterpansong.mp3");
+    song2=loadSound("harrypottersong.mp3");
 }
 function setup(){
-canvas=createCanvas(600,450);
+canvas=createCanvas(500,350);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
 }
 function draw(){
-    image(video,0,0,600,450);
+    image(video,0,0,500,350);
 }
 function play(){
     song.play();
